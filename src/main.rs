b470e0fa91fb9ad95649ab0fb9d82e6dd78da2d4
JereSalo <jeresalo17@gmail.com>
2024-09-03 16:40:58 -0300

    Methods for Grid, thinking on data structures

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,77 @@
-use std::arch::aarch64::int16x4_t;
+
 
 use macroquad::prelude::*;
 
+#[derive(Debug, Clone, Copy)]
 enum Cell{
     Dead,
     Alive
 }
 
 impl Cell{
-
     // Gets alive neighbors of the cell (it's a number between 0 and 8)
     fn get_alive_neighbors(&self) -> u8 {
         todo!();
     }
+
+    fn next_state(&self) -> Self{
+        todo!("")
+    }
 }
 
+#[derive(Debug)]
+struct Grid{
+    cells: Vec<Vec<Cell>>,
+    next_gen: Vec<Vec<Cell>>,
+}
+
+impl Default for Grid{
+    fn default() -> Self {
+        Grid { cells: vec![
+            vec![Cell::Dead, Cell::Alive, Cell::Dead, Cell::Dead],
+            vec![Cell::Alive, Cell::Alive, Cell::Dead, Cell::Dead],
+            vec![Cell::Dead, Cell::Dead, Cell::Alive, Cell::Dead],
+            vec![Cell::Dead, Cell::Dead, Cell::Dead, Cell::Alive],
+        ], next_gen: vec![vec![Cell::Dead; 4];4] }
+    }
+}
+
+impl Grid{
+    fn new(rows: usize, cols: usize, default_value: Cell) -> Self {
+        Grid { 
+            cells: vec![vec![default_value; cols]; rows], 
+            next_gen: vec![vec![Cell::Dead; cols];rows]
+        }
+    }
+
+    fn new_preloaded() -> Self{
+        Self::default()
+    }
+
+    fn neighbors(x: u64, y: u64) -> Vec<Cell> {
+        todo!();
+    }
+
+
+}
 
 #[macroquad::main("Conway's Game of Life")]
 async fn main() {
+    clear_background(BLACK);
+    let rows = 4;
+    let cols = 4;
+
+    let grid = Grid::new_preloaded();
     loop {
-        clear_background(BLACK);
 
-        // Cell: Enum?
-        //  Methods: next_state (calculates how many neighbors alive and decides whether or not it lives or dies)
+        
 
-        // Grid: 100x100 grid? From [0][0] to [99][99]
-        //  It has cells in it, on each space available there is a cell
-        //  You can ask to it for the neighbors of a cell and it will return 8 cells (?)
+        println!("{:?}", grid);
+        break;
+
+        // 
+
+
 
         next_frame().await
     }
