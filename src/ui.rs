17e34060e9eea9b90137d56dc8eda4b243609ac7
JereSalo <jeresalo17@gmail.com>
2024-09-06 12:45:51 -0300

    add more abstractions to UI

--- src/ui.rs
+++ src/ui.rs
@@ -23,42 +23,14 @@ impl UI{
     pub fn run(&mut self){
         clear_background(BLACK);
 
-        // Width and Height are defined on every tick cause screen can be resized.
-        self.scr_w = screen_width();
-        self.scr_h = screen_height();
-        self.cell_w = self.scr_w / self.grid.cols() as f32;
-        self.cell_h = self.scr_h / self.grid.rows() as f32;
+        self.get_screen_and_cell_size();
 
         self.draw_board();
 
-        // Handle KeyPress: R (Random), C (Clear), Space (Play/Pause), I (Toggle Info), Q (Quit)
-        match get_last_key_pressed() {
-            Some(KeyCode::R) => self.grid = Grid::new_random(ROWS, COLS),
-            Some(KeyCode::C) => self.grid = Grid::new(ROWS, COLS),
-            Some(KeyCode::Space) => self.paused = !self.paused,
-            Some(KeyCode::I) => self.show_info = !self.show_info,
-            Some(KeyCode::Q) => process::exit(0),
-            Some(_) | None => {}
-        }
-
+        self.handle_key_press();
+        
         if self.show_info {
-            let info = "'I' to toggle info on/off\n\
-            'R' to generate random grid\n\
-            'C' to clear the grid\n\
-            'Space' to play/pause\n\
-            'Click' to kill/revive cells\n\
-            'Q' to quit the game";
-            // Show key bindings
-
-            draw_rectangle(self.scr_w * 0.2, self.scr_h * 0.7, self.scr_w * 0.6, self.scr_h * 0.26, BLACK);
-            draw_multiline_text(
-                info,
-                self.scr_w * 0.25,
-                self.scr_h * 0.75,
-                self.scr_h * 0.034,
-                Some(self.scr_h / 800.),
-                WHITE,
-            );
+            self.show_info();
         }
 
         if !self.paused {
@@ -68,15 +40,7 @@ impl UI{
             // Just to show that it's paused without being invasive.
             draw_hexagon(20.0, 20.0, 15.0, 2.0, true, RED, RED);
 
-            // Let user kill/revive cells
-            if is_mouse_button_released(MouseButton::Left) {
-                let (x, y) = mouse_position();
-
-                let i = (y / self.cell_h) as usize;
-                let j = (x / self.cell_w) as usize;
-
-                self.grid.cells[i][j] = !self.grid.cells[i][j];
-            }
+            self.handle_cell_interaction();
         }
     }
 
@@ -88,7 +52,7 @@ impl UI{
              scr_h: 0.,
              cell_w: 0.,
              cell_h: 0.
-            }
+        }
     }
 
     fn draw_board(&self){
@@ -105,11 +69,55 @@ impl UI{
         }
     }
 
-    fn handle_key_press(){
-
+    fn get_screen_and_cell_size(&mut self){
+        // Width and Height are defined on every tick cause screen can be resized.
+        self.scr_w = screen_width();
+        self.scr_h = screen_height();
+        self.cell_w = self.scr_w / self.grid.cols() as f32;
+        self.cell_h = self.scr_h / self.grid.rows() as f32;
     }
 
-    fn show_info(){
+    fn handle_key_press(&mut self){
+        // Handle KeyPress: R (Random), C (Clear), Space (Play/Pause), I (Toggle Info), Q (Quit)
+        match get_last_key_pressed() {
+            Some(KeyCode::R) => self.grid = Grid::new_random(ROWS, COLS),
+            Some(KeyCode::C) => self.grid = Grid::new(ROWS, COLS),
+            Some(KeyCode::Space) => self.paused = !self.paused,
+            Some(KeyCode::I) => self.show_info = !self.show_info,
+            Some(KeyCode::Q) => process::exit(0),
+            Some(_) | None => {}
+        }
+    }
 
+    fn show_info(&mut self){
+        let info = "'I' to toggle info on/off\n\
+        'R' to generate random grid\n\
+        'C' to clear the grid\n\
+        'Space' to play/pause\n\
+        'Click' to kill/revive cells\n\
+        'Q' to quit the game";
+        // Show key bindings
+
+        draw_rectangle(self.scr_w * 0.2, self.scr_h * 0.7, self.scr_w * 0.6, self.scr_h * 0.26, BLACK);
+        draw_multiline_text(
+            info,
+            self.scr_w * 0.25,
+            self.scr_h * 0.75,
+            self.scr_h * 0.034,
+            Some(self.scr_h / 800.),
+            WHITE,
+        );
+    }
+
+    fn handle_cell_interaction(&mut self){
+        // Let user kill/revive cells
+        if is_mouse_button_released(MouseButton::Left) {
+            let (x, y) = mouse_position();
+
+            let i = (y / self.cell_h) as usize;
+            let j = (x / self.cell_w) as usize;
+
+            self.grid.cells[i][j] = !self.grid.cells[i][j];
+        }
     }
 }
