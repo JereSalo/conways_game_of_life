40cd0088e438cd966a30b99d173800ccffe686db
JereSalo <jeresalo17@gmail.com>
2024-09-06 12:29:39 -0300

    abstract draw_board and change UI struct attributes

--- src/ui.rs
+++ src/ui.rs
@@ -12,7 +12,11 @@ const COLS: usize = 50;
 pub struct UI{
     grid: Grid,
     paused: bool,
-    show_info: bool
+    show_info: bool,
+    scr_w: f32,
+    scr_h: f32,
+    cell_w: f32,
+    cell_h: f32,
 }
 
 impl UI{
@@ -20,22 +24,12 @@ impl UI{
         clear_background(BLACK);
 
         // Width and Height are defined on every tick cause screen can be resized.
-        let scr_w = screen_width();
-        let scr_h = screen_height();
-        let cell_w = scr_w / self.grid.cols() as f32;
-        let cell_h = scr_h / self.grid.rows() as f32;
+        self.scr_w = screen_width();
+        self.scr_h = screen_height();
+        self.cell_w = self.scr_w / self.grid.cols() as f32;
+        self.cell_h = self.scr_h / self.grid.rows() as f32;
 
-        // Draw Board
-        for i in 0..self.grid.rows() {
-            for j in 0..self.grid.cols() {
-                if self.grid.cells[i][j] == Cell::Alive {
-                    draw_rectangle(cell_w * j as f32, cell_h * i as f32, cell_w, cell_h, WHITE);
-                }
-            }
-            // Draw vertical an horizontal lines of grid
-            draw_line(0.0, cell_h * i as f32, scr_w, cell_h * i as f32, 1.0, GRAY);
-            draw_line(cell_w * i as f32, 0.0, cell_w * i as f32, scr_h, 1.0, GRAY);
-        }
+        self.draw_board();
 
         // Handle KeyPress: R (Random), C (Clear), Space (Play/Pause), I (Toggle Info), Q (Quit)
         match get_last_key_pressed() {
@@ -56,13 +50,13 @@ impl UI{
             'Q' to quit the game";
             // Show key bindings
 
-            draw_rectangle(scr_w * 0.2, scr_h * 0.7, scr_w * 0.6, scr_h * 0.26, BLACK);
+            draw_rectangle(self.scr_w * 0.2, self.scr_h * 0.7, self.scr_w * 0.6, self.scr_h * 0.26, BLACK);
             draw_multiline_text(
                 info,
-                scr_w * 0.25,
-                scr_h * 0.75,
-                scr_h * 0.034,
-                Some(scr_h / 800.),
+                self.scr_w * 0.25,
+                self.scr_h * 0.75,
+                self.scr_h * 0.034,
+                Some(self.scr_h / 800.),
                 WHITE,
             );
         }
@@ -78,8 +72,8 @@ impl UI{
             if is_mouse_button_released(MouseButton::Left) {
                 let (x, y) = mouse_position();
 
-                let i = (y / cell_h) as usize;
-                let j = (x / cell_w) as usize;
+                let i = (y / self.cell_h) as usize;
+                let j = (x / self.cell_w) as usize;
 
                 self.grid.cells[i][j] = !self.grid.cells[i][j];
             }
@@ -87,6 +81,35 @@ impl UI{
     }
 
     pub fn new() -> Self{
-        UI {grid: Grid::new(ROWS,COLS), paused: true, show_info: true}
+        UI {grid: Grid::new(ROWS,COLS),
+             paused: true, 
+             show_info: true, 
+             scr_w: 0.,
+             scr_h: 0.,
+             cell_w: 0.,
+             cell_h: 0.
+            }
+    }
+
+    fn draw_board(&self){
+          // Draw Board
+          for i in 0..self.grid.rows() {
+            for j in 0..self.grid.cols() {
+                if self.grid.cells[i][j] == Cell::Alive {
+                    draw_rectangle(self.cell_w * j as f32, self.cell_h * i as f32, self.cell_w, self.cell_h, WHITE);
+                }
+            }
+            // Draw vertical an horizontal lines of grid
+            draw_line(0.0, self.cell_h * i as f32, self.scr_w, self.cell_h * i as f32, 1.0, GRAY);
+            draw_line(self.cell_w * i as f32, 0.0, self.cell_w * i as f32, self.scr_h, 1.0, GRAY);
+        }
+    }
+
+    fn handle_key_press(){
+
+    }
+
+    fn show_info(){
+
     }
 }
